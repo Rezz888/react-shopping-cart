f3f35f3b7a553e5f09966f881e12d70b0f87d22f
Rezaur Rahman <rezaur.rahman_nyc@icloud.com>
2021-01-08 00:02:43 -0500

    started

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import data from './data';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
-
+// just started project
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
